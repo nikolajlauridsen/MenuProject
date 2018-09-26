af065edce00ff7f9dae468cb21027d48e3962118
Molex <simplesleeptimer@gmail.com>
2018-09-26 08:53:37 +0200

    Finish up LoadMenu

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -8,6 +8,10 @@ namespace SmartMenuLibrary
 {
     public class SmartMenu
     {
+        private String MenuTitle;
+        private String InputDescription;
+        private string[,] menuPoints;
+
         public void LoadMenu(string path)
         {
             // Read the text file
@@ -18,6 +22,25 @@ namespace SmartMenuLibrary
             System.IO.StreamReader textFile = new System.IO.StreamReader(path);
             while((line = textFile.ReadLine()) != null) {
                 lines.Add(line);
+            }
+
+            MenuTitle = lines[0];
+            InputDescription = lines[1];
+
+            menuPoints = new string[lines.Count - 2, 2];
+            for(int i=2; i < lines.Count; i++) {
+                string[] split = lines[i].Split(';');
+                menuPoints[i - 2, 0] = split[0];
+                menuPoints[i - 2, 1] = split[1];
+            }
+
+            // Test if it works.
+            Console.WriteLine("Menu title: " + MenuTitle);
+            Console.WriteLine("Desc: " + InputDescription);
+            Console.WriteLine("Menu points: ");
+            for(int i=0; i < menuPoints.GetLength(0); i++) {
+                Console.WriteLine("Point title: " + menuPoints[i, 0]);
+                Console.WriteLine("Point id: " + menuPoints[i, 1]);
             }
         }
         public void Activate()
